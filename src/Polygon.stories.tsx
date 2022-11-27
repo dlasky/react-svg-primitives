cbc628f7ae19f4c3aa096ec25b7338f67b17a3d5
dlasky <github@aerolith.net>
2022-11-27 10:39:01 -0500

    line

--- src/Polygon.stories.tsx
+++ src/Polygon.stories.tsx
@@ -1,11 +1,17 @@
-import Polygon from "./Polygon"
+import { Polygon, Polyline } from "./Polygon"
 import React from "react"
 import { SVG } from "./SVG"
 
 export const PolyString = () => {
-    return <SVG><Polygon points="1,1 10,1 10,11 1,11"/></SVG>
+    return <SVG>
+        <Polygon points="1,1 10,1 10,11 1,11" />
+        <Polyline points="20,20 30,20, 30,30 20,30 20,20" />
+    </SVG>
 }
 
 export const PolyPoints = () => {
-    return <SVG><Polygon points={[{x:1,y:1},{x:10,y:1},{x:10,y:11},{x:1,y:11}]}/></SVG>
+    return <SVG>
+        <Polygon points={[{ x: 1, y: 1 }, { x: 10, y: 1 }, { x: 10, y: 11 }, { x: 1, y: 11 }]} />
+        <Polyline style={{ stroke: "black" }} points={[{ x: 15, y: 15 }, { x: 30, y: 30 }]} />
+    </SVG>
 }
